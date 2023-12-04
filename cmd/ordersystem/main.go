44cccb01a14323dbb1ce998cf0fb6e0fef3e5a8f
Christian Ferraz <christianferraz@gmail.com>
2023-12-03 21:37:23 -0400

    db.Exec

--- cmd/ordersystem/main.go
+++ cmd/ordersystem/main.go
@@ -32,7 +32,7 @@ func main() {
 		panic(err)
 	}
 	defer db.Close()
-
+	db.Exec("CREATE TABLE IF NOT EXISTS orders (id varchar(255) NOT NULL, price float NOT NULL, tax float NOT NULL, final_price float NOT NULL, PRIMARY KEY (id))")
 	rabbitMQChannel := getRabbitMQChannel(configs.RabbitMQHost)
 
 	eventDispatcher := events.NewEventDispatcher()
